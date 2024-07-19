417707730659ca253075fe50cf1bb79f4dfaec9a
Rafael Santos Cunha <rafaelsantoscunha02@gmail.com>
2024-07-19 12:35:04 -0300

    Creation of the navbar.
    Criação da navbar.

--- erp-front-angular/src/app/app.module.ts
+++ erp-front-angular/src/app/app.module.ts
@@ -1,18 +1,23 @@
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CategoriesComponent } from './categories/categories.component';
+import { NavComponent } from './nav/nav.component';
+
 
 @NgModule({
-  declarations: [
+  declarations: [	
     AppComponent,
-    CategoriesComponent
-  ],
+    CategoriesComponent,
+      NavComponent
+   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
